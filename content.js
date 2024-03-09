457551fd166a0726a834db36faf2269159cb0bbb
aka272 <aka272@sfu.ca>
2024-03-09 15:03:41 -0800

    edited typo

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 const script = document.createElement('script');
-script.scr = "https:??maps.googleapis.com/maps/api/js?key=AIzaSyAbauG84e4IlL78ZiAmnx0DpcFiowviJKs";
+script.src = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"; 
 document.head.appendChild(script);
 
-function initMap(){
+function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: { lat: -34.397, lng: 150.644 }
