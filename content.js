c17a0d2bcfd1fe74154d112a50bae08a35b5557c
aka272 <aka272@sfu.ca>
2024-03-09 14:44:50 -0800

    added API key for google maps

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-console.log("Hello from Google Maps Special Function Extension!");
+const script = document.createElement('script');
+script.scr = "https:??maps.googleapis.com/maps/api/js?key=AIzaSyAbauG84e4IlL78ZiAmnx0DpcFiowviJKs";
+document.head.appendChild(script);
+
+function initMap(){
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: { lat: -34.397, lng: 150.644 }
+    });
+    
+    const marker = new google.maps.Marker({
+        position: { lat: -34.397, lng: 150.644 },
+        map: map
+    });
+}
